53731ec71c12c7b9430af7bd2464007fc192f6ee
AlaminPu1007 <alamin66.sit@gmail.com>
2022-05-14 22:11:51 +0600

    Day of the programmers algorithm problem solved

---
+++ Hacker rank/Algorithms/problem-5/Day_Of_The_programmers.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+int main()
+{
+    int i, year, sumOfMonth = 0, checkLeapYear = 0;
+    scanf("%d",&year);
+
+    if(year < 1918){
+        if(year % 4==0)printf("12.09.%d\n",year);
+        else printf("13.09.%d\n",year);
+    }
+    else if(year == 1918){
+        printf("26.09.%d\n",year);
+    }
+    else{
+        if(year % 400 == 0){
+            printf("12.09.%d\n",year);
+        }
+        else if(year % 4== 0 && year % 100 != 0){
+            printf("12.09.%d\n",year);
+        }
+        else  printf("13.09.%d\n",year);
+    }
+
+    return 0;
+}

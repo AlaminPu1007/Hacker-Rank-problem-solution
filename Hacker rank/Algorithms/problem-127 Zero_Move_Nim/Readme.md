4480b202e68cdc3d91062317c54f4dbe69edafd3
AlaminPu1007 <alamin66.sit@gmail.com>
2022-12-07 20:59:50 +0600

    Zero move problem solved

---
+++ Hacker rank/Algorithms/problem-127 Zero_Move_Nim/Readme.md
@@ -0,0 +1,13 @@
+## Zero-Move Nim
+
+[Visit problem](https://www.hackerrank.com/challenges/zero-move-nim/problem)
+
+**Approach :**<br>
+
+-   xor each item if it's `(x % 2 !== 0) by summation 1`
+-   else xor by subtraction 1
+
+**Complexity Analysis :**<br>
+
+-   Time-Complexity: `O(n)`
+-   Space-Complexity: `O(1)`
---
+++ Hacker rank/Algorithms/problem-127 Zero_Move_Nim/Zero_Move_Nim.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int g, n, x, s;
+    scanf("%d", &g);
+    while (g--)
+    {
+        scanf("%d", &n);
+        s = 0;
+        while (n--)
+        {
+            scanf("%d", &x);
+            if (x % 2)
+                s ^= x + 1;
+            else
+                s ^= x - 1;
+        }
+        if (s)
+            printf("W\n");
+        else
+            printf("L\n");
+    }
+    return 0;
+}

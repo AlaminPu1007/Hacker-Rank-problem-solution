224295f5d8ab2fef4bb205c1a610dcd66e06aa4a
alaminpu1007 <alamin66.sit@gmial.com>
2023-03-14 09:09:54 +0600

    modified problem-56

--- Hacker rank/Algorithms/problem-56/The_Love_Letter_Mystery.c
+++ Hacker rank/Algorithms/problem-56/The_Love_Letter_Mystery.c
@@ -7,37 +7,35 @@ int main(void)
     int t;
     scanf("%d", &t);
 
-    while(t > 0)
+    while (t > 0)
     {
         char str[10000];
 
-        scanf("%s",str);
+        scanf("%s", str);
 
         int len = 0;
 
         len = strlen(str);
         int j = len - 1, value = 0, i;
-        for(i = 0; i < len/2; j--, i++)
+        for (i = 0; i < len / 2; j--, i++)
         {
-            if(str[i] != str[j])
+            if (str[i] != str[j])
             {
                 value += abs(str[i] - str[j]);
             }
         }
 
-        if(value)
+        if (value)
         {
-            printf("%d\n",value);
+            printf("%d\n", value);
         }
-        else {
+        else
+        {
             printf("0\n");
         }
 
         t--;
     }
-
-
-
 
     return 0;
 }
